ccb2852b872dcb1505d69e0a0c1f43ec47d57221
matteofresta <matteofresta@hotmail.com>
2025-10-23 16:53:16 +0200

    23.10.2025: Burger Menu added for mobile devices / ui improvement

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,68 +1,96 @@
 import { useTranslation } from "react-i18next";
 import { columnConfig } from "./config/columnConfig";
+import Hamburger from 'hamburger-react'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
 } from "@radix-ui/react-dropdown-menu";
 import { motion } from "framer-motion";
 import { LoadingSkeleton } from "@/components/ui/shadcn/skeleton";
+import { useBurgerMenu } from "@/hooks/BurgerMenu/useBurgerMenu.ts";
 
 export const Navbar = () => {
-  const { t, i18n, ready } = useTranslation();
+    const { t, i18n, ready } = useTranslation();
 
-  return (
-    <>
-      <div className="w-full h-24 shadow-2xl flex justify-between items-center px-10">
-        <div></div>
-        <div></div>
-        <div className="flex items-center gap-10">
-          {ready ? (
-            <div>
-              <ul className="flex gap-4">
-                {columnConfig.map((item) => (
-                  <li key={item.key}>
-                    <a className="text-black text-md" href={item.path}>
-                      {t(item.label)}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+    const {isOpen, setOpen} = useBurgerMenu();
+
+    return (
+        <>
+            <div className="w-full h-24 shadow-2xl flex justify-between items-center px-10">
+                <div></div>
+                <div></div>
+                <div className="flex items-center gap-10">
+                    <div className="hidden md:flex items-center gap-10">
+                        {ready ? (
+                            <ul className="flex gap-4">
+                                {columnConfig.map((item) => (
+                                    <li key={item.key}>
+                                        <a className="text-black text-md" href={item.path}>
+                                            {t(item.label)}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <LoadingSkeleton variant={"NavLinks"} />
+                        )}
+                    </div>
+
+                    <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="bg-white/80 hover:scale-105 duration-300 cursor-pointer backdrop-blur-sm border border-gray-300/50 shadow-sm rounded-md px-4 py-2 w-24 text-center">
+                                {i18n.language.toUpperCase()}
+                            </DropdownMenuTrigger>
+
+                            <DropdownMenuContent className="bg-gray-500/20 backdrop-blur-lg mt-2 rounded-xl flex flex-col gap-2 border-none shadow-2xl w-40 p-2">
+                                <DropdownMenuItem
+                                    className="focus:bg-white/20 bg-white/80 rounded-md duration-400 p-2 cursor-pointer"
+                                    onSelect={() => i18n.changeLanguage("de")}
+                                >
+                                    Deutsch
+                                </DropdownMenuItem>
+                                <DropdownMenuItem
+                                    className="focus:bg-white/20 bg-white/80 rounded-md duration-400 p-2 cursor-pointer"
+                                    onSelect={() => i18n.changeLanguage("en")}
+                                >
+                                    English
+                                </DropdownMenuItem>
+                                <DropdownMenuItem
+                                    className="focus:bg-white/20 bg-white/80 rounded-md duration-400 p-2 cursor-pointer"
+                                    onSelect={() => i18n.changeLanguage("it")}
+                                >
+                                    Italiano
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </motion.div>
+
+                    <div className="md:hidden">
+                        <Hamburger toggled={isOpen} toggle={setOpen} direction="right" />
+                    </div>
+                </div>
             </div>
-          ) : (
-            <LoadingSkeleton variant={"NavLinks"} />
-          )}
-          <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
-            <DropdownMenu>
-              <DropdownMenuTrigger className="bg-white/80 hover:scale-105 duration-300 cursor-pointer backdrop-blur-sm border border-gray-300/50 shadow-sm rounded-md px-4 py-2 w-24 text-center">
-                {i18n.language.toUpperCase()}
-              </DropdownMenuTrigger>
-
-              <DropdownMenuContent className="bg-gray-500/20 backdrop-blur-lg mt-2 rounded-xl flex flex-col gap-2 border-none shadow-2xl w-40 p-2">
-                <DropdownMenuItem
-                  className="focus:bg-white/20 bg-white/80 rounded-md duration-400 p-2 cursor-pointer"
-                  onSelect={() => i18n.changeLanguage("de")}
-                >
-                  Deutsch
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  className="focus:bg-white/20 bg-white/80 rounded-md duration-400 p-2 cursor-pointer"
-                  onSelect={() => i18n.changeLanguage("en")}
-                >
-                  English
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  className="focus:bg-white/20 bg-white/80 rounded-md duration-400 p-2 cursor-pointer"
-                  onSelect={() => i18n.changeLanguage("it")}
-                >
-                  Italiano
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </motion.div>
-        </div>
-      </div>
-    </>
-  );
+            {isOpen && (
+                <div className="md:hidden bg-white/80">
+                    {ready ? (
+                        <ul className="flex flex-col gap-4 items-center py-14">
+                            {columnConfig.map((item) => (
+                                <li key={item.key}>
+                                    <a className="text-black text-2xl" href={item.path}>
+                                        {t(item.label)}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <div className="p-4">
+                            <LoadingSkeleton variant={"NavLinks"} />
+                        </div>
+                    )}
+                </div>
+            )}
+        </>
+    );
 };
---
+++ src/hooks/BurgerMenu/useBurgerMenu.ts
@@ -0,0 +1,8 @@
+import {useState} from "react";
+
+export const useBurgerMenu = () => {
+    const [isOpen, setOpen] = useState(false)
+
+
+    return {isOpen, setOpen};
+};
